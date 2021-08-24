720b94baf435d5d03b5d10ecfb837d4c8facba69
Jeff Wheeler <jeff@1wheeler.com>
2021-08-24 10:50:29 -0600

    Update main.tf

--- jw/main.tf
+++ jw/main.tf
@@ -44,6 +44,7 @@ variable "morpheususer" {
 resource "aws_instance" "morph_tf_ec2"{
   ami           = local.amis.ubuntu.us-east-1
   instance_type = "t3.micro"
+  key_name= "jwheeler"
   associate_public_ip_address = true
   user_data = <<-EOF
    #!/bin/bash
