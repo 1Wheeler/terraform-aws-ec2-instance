af8cdb2e4ac261850caad46c0d2d3fa31fd43cfe
Jeff Wheeler <jeff@1wheeler.com>
2021-08-27 11:31:14 -0600

    Update main.tf

--- jw/main.tf
+++ jw/main.tf
@@ -53,7 +53,7 @@ resource "aws_instance" "jwec2ubuntu" {
   instance_type = "t3.micro"
   vpc_id = data.aws_vpc.selected.id
   subnet_id = "<%customOptions.networksApiExternal%>"
-  vpc_security_group_ids = [<%customOptions.securityGroupsApiExternal%>]
+  vpc_security_group_ids = "[<%customOptions.securityGroupsApiExternal%>]"
   associate_public_ip_address = true
   key_name = "jwheeler"
     user_data = <<-EOF
